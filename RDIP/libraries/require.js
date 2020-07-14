3b46d8f0f055dd612a7e132e68742bc0b5ec480f
DebabrataMaity <46934864+DebabrataMaity@users.noreply.github.com>
2020-07-14 19:09:30 +0530

    Update require.js

--- RDIP/libraries/require.js
+++ RDIP/libraries/require.js
@@ -1,1 +1,51 @@
 
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app'
+    }
+});
+
+// Start the main app logic.
+requirejs(['jquery', 'canvas', 'app/sub'],
+function   ($,        canvas,   sub) {
+    //jQuery, canvas and the app/sub module are all
+    //loaded and can be used here now.
+});
+
+
+//A module definition inside foo/title.js. It uses
+//my/cart and my/inventory modules from before,
+//but since foo/title.js is in a different directory than
+//the "my" modules, it uses the "my" in the module dependency
+//name to find them. The "my" part of the name can be mapped
+//to any directory, but by default, it is assumed to be a
+//sibling to the "foo" directory.
+define(["my/cart", "my/inventory"],
+    function(cart, inventory) {
+        //return a function to define "foo/title".
+        //It gets or sets the window title.
+        return function(title) {
+            return title ? (window.title = title) :
+                   inventory.storeName + ' ' + cart.name;
+        }
+    }
+);
+
+
+//Inside b.js:
+define(["require", "a"],
+    function(require, a) {
+        //"a" in this case will be null if "a" also asked for "b",
+        //a circular dependency.
+        return function(title) {
+            return require("a").doSomething();
+        }
+    }
+);
